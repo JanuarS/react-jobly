7326ee5665cd0c532de2acfb22bff652d594d2a1
Januar Soepangat <januar.soepangat@gmail.com>
2022-08-04 05:19:42 -0500

    add SearchForm

---
+++ frontend/src/components/SearchForm/index.js
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+import "./style.css";
+
+function SearchForm({ searchFor }) {
+
+  return (
+    <div>
+      <form>
+        <input />
+        <button type="submit">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default SearchForm;
