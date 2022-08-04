335794ca65fd17399bf5f349826cfd27eb8a5b34
Januar Soepangat <januar.soepangat@gmail.com>
2022-08-04 07:59:10 -0500

    fix Search bug

--- frontend/src/components/SearchForm/index.js
+++ frontend/src/components/SearchForm/index.js
@@ -2,12 +2,29 @@ import React, { useState } from "react";
 import "./style.css";
 
 function SearchForm({ searchFor }) {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  function handleSubmit(evt) {
+    evt.preventDefault();
+    searchFor(searchTerm);
+    setSearchTerm(searchTerm);
+  }
+
+  function handleChange(evt) {
+    setSearchTerm(evt.target.value);
+  }
 
   return (
     <div>
-      <form>
-        <input />
-        <button type="submit">
+      <form className="" onSubmit={handleSubmit}>
+        <input 
+          className="form-control"
+          name="searchTerm"
+          placeholder="Enter search term"
+          value={searchTerm}
+          onChange={handleChange}
+        />
+        <button className="btn btn-primary" type="submit">
           Submit
         </button>
       </form>
