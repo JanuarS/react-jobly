9b5ce857ce8468ba68e78c57a44b1ae2e461d821
Januar Soepangat <januar.soepangat@gmail.com>
2022-08-04 07:59:54 -0500

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter } from "react-router-dom";
 
 import Homepage from './pages/Homepage';
 import Companies from './pages/Companies';
+import NavBar from './components/Navbar';
 
 
 import logo from './logo.svg';
@@ -12,6 +13,7 @@ function App() {
   return (
     <BrowserRouter>
       <Homepage />
+      <NavBar />
       <Companies />
     </BrowserRouter>
   );
