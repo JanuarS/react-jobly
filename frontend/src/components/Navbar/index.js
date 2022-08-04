eea1ffd410d9894316732f29f512dcf923cdda08
Januar Soepangat <januar.soepangat@gmail.com>
2022-08-04 07:59:40 -0500

    update Navbar

---
+++ frontend/src/components/Navbar/index.js
@@ -0,0 +1,16 @@
+import React, { useContext } from "react";
+import { Link, NavLink } from "react-router-dom";
+
+
+import "./style.css";
+
+function NavBar({ }) {
+
+  return (
+    <nav className="">
+      <h1>Navbar</h1>
+    </nav>
+  );
+}
+
+export default NavBar;
